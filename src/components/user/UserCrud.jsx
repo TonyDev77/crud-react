92207fca35c21cd7b28e6473eaf174a81fbe5139
TonyDev77 <tony.music7@gmail.com>
2021-04-05 16:39:35 -0300

    Responsive design implemented

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -116,12 +116,12 @@ class UserCrud extends Component {
         return (
             <table className="table mt-4">
                 <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Ações</th>
-                </tr>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
                 </thead>
                 <tbody>{ this.renderRows() }</tbody>
             </table>
